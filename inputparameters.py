78d89c764d6b59d93e28429fbdef166d1e17c7c9
Mohamed Elkamash <me21@illinois.edu>
2023-11-01 12:18:05 -0500

    fixed definition of diffusivity

--- inputparameters.py
+++ inputparameters.py
@@ -1,10 +1,10 @@
 from math import pi
 
-D = 0.06         #m
-R = D/2          #m
-k = 15           #W.m^-1.K-1
-T0 = 500         #degC
-rho = 8000       #kg.m^-3
-Cp = 500         #J.kg^-1.K^-1
-alpha = rho*Cp/k #m^2.s^-1
+D = 0.06           #m
+R = D/2            #m
+k = 15             #W.m^-1.K-1
+T0 = 500           #degC
+rho = 8000         #kg.m^-3
+Cp = 500           #J.kg^-1.K^-1
+alpha = k/(rho*Cp) #m^2.s^-1
 
--- main.py
+++ main.py
@@ -8,9 +8,14 @@ n_elements = 100
 n_terms = 10
 r = np.linspace(1e-10, R, n_elements)
 
-T_anal = computeAnalyticalTempDistr(r, 10, n_terms)
-
+T_anal = computeAnalyticalTempDistr(r, 0, n_terms)
 plt.plot(r, T_anal)
+T_anal = computeAnalyticalTempDistr(r, 1000, n_terms)
+plt.plot(r, T_anal)
+""" T_anal = computeAnalyticalTempDistr(r, 200, n_terms)
+plt.plot(r, T_anal)
+T_anal = computeAnalyticalTempDistr(r, 300, n_terms)
+plt.plot(r, T_anal) """
 plt.show()
 
 
