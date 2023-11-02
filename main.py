b07b46752ac29ae91274bdee2b0836ffa501c75c
Mohamed Elkamash <me21@illinois.edu>
2023-11-01 21:03:45 -0500

    added finite difference

--- main.py
+++ main.py
@@ -1,9 +1,44 @@
-from inputparameters import R
+from inputparameters import R, alpha, T0
 from analyticaltemp import AnalyticalTemperature
 import numpy as np
 import matplotlib.pyplot as plt
 
-m_to_cm = 100
+n_elements = 20
+delta_r = R/n_elements
+r = np.linspace(0, R, n_elements+1)
+delta_t = 0.1
+t_max = 60
+t = np.linspace(0, t_max, int(t_max/delta_t) + 1)
+
+T = np.empty((len(r), len(t)))
+
+b = alpha*delta_t/delta_r
+
+#initial condition
+T[:,0] = T0/2 * (1 - np.cos(np.pi * r / R))
+
+for l in range(1, len(t)):
+    #interior points
+    for i in range(1, len(r)-1):
+        T[i,l] = T[i,l-1] + b/delta_r * (T[i+1,l-1] - 2*T[i,l-1] + T[i-1,l-1]) + b/r[i] * (T[i+1,l-1] - T[i-1,l-1])
+    
+    #boundary points
+    I = len(r) - 1
+    T[0,l] = T[0,l-1] + 2*b/delta_r * (T[1,l-1] - T[0,l-1])
+    T[I,l] = T[I,l-1] + 2*b/delta_r * (T[I-1,l-1] - T[I,l-1])
+
+plt.plot(r, T[:,600])
+plt.yticks(np.arange(0, 501, step=100))
+plt.show() 
+
+
+
+
+
+
+
+
+""" m_to_cm = 100
 n_elements = 100
 n_terms = 5
 n_timestamps = 7
@@ -50,3 +85,4 @@ plt.yticks(np.arange(0, 501, step=100))
 plt.savefig("T(r).png", dpi = 400)
 
 
+ """
